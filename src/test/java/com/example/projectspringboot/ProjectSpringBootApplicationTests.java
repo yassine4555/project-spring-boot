a16eb8646cab9e1c9560523bf5819acd5edbe58b
yassine4555 <yasso20032004@gmail.com>
2025-05-04 17:59:04 +0100

    first commit

---
+++ src/test/java/com/example/projectspringboot/ProjectSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
